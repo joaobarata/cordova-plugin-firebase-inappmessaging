c3cd3ad03d82179fca84d0390234b9062c18bdc3
João Barata <joao.barata@outsystems.com>
2022-03-07 17:33:22 +0000

    Try to to a pod repo update

--- hooks/updatePodSpec.js
+++ hooks/updatePodSpec.js
@@ -2,7 +2,7 @@ console.log("Running hook to update podspecs");
 module.exports = function (context) {
   return new Promise((resolve, reject) => {
     var child_process = require('child_process');
-    child_process.exec('pod install --repo-update', {cwd: __dirname}, function (error) {
+    child_process.exec('pod repo update', {cwd: __dirname}, function (error) {
       if (error !== null) {
         console.log('exec error: ' + error);
         reject('podspec update failed');
